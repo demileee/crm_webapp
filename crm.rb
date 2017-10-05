932c83a1ac28fda7e306fa65a3f927564567b335
Demi Lee <demilee.to@gmail.com>
2017-10-05 12:03:56 -0400

    add, edit, delete contact pages

--- crm.rb
+++ crm.rb
@@ -10,10 +10,67 @@ get '/home' do
   erb :index
 end
 
-get '/contacts/:id' do
+get '/contact/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
   if @contact
     erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/add_contact' do
+  erb :add_contact
+end
+
+post '/add_contact' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/home')
+end
+
+get '/contact/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contact/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+    redirect to ('/home')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contact/:id/delete' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :delete_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contact/:id/delete' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/home')
   else
     raise Sinatra::NotFound
   end
